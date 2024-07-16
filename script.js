4878dea829338f9dfbc9fa90c3c16a03028efcc4
HWIBUH <evoriusvalens@gmail.com>
2024-07-16 15:57:15 +0700

    Teach me how to make a Calculator pls

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
-class Calculator {
-    constructor()
-}
+
+//console.log(document.querySelectorAll("p"))
+
+let number = document.querySelectorAll("#number")
+let screen = document.querySelector("#screen")
+let backspace = document.querySelector("#ac")
+let operator = document.querySelectorAll("#operator")
+let result = document.querySelector(".result")
+let percentage = document.querySelector("#percentage")
+let plusmin = document.querySelector("#plusmin")
+let ope = ""
+let firstNumber = undefined
+
+number.forEach((num) => {
+    num.addEventListener('click', () => {
+        screen.innerHTML += num.innerHTML
+    })
+})
+
+backspace.addEventListener('click', () => {
+    screen.innerHTML = screen.innerHTML.substring(0, screen.innerHTML.length-1)
+})
+
+operator.forEach((oper) => {
+    oper.addEventListener('click', () => {
+        firstNumber = screen.innerHTML
+        screen.innerHTML = ""
+        ope = oper.innerHTML
+        console.log(ope)
+    })
+})
+
+percentage.addEventListener('click', () => {
+    screen.innerHTML = screen.innerHTML*0.01
+})
+
+plusmin.addEventListener('click', () => {
+    screen.innerHTML = screen.innerHTML*-1
+})
+
+result.addEventListener('click', () => {
+    switch(ope) {
+        case "-" :
+            screen.innerHTML = firstNumber - screen.innerHTML
+            break
+        case "+" :
+            screen.innerHTML = firstNumber + screen.innerHTML
+            break
+        case "x" :
+            screen.innerHTML = firstNumber * screen.innerHTML
+            break
+        case "/" :
+            screen.innerHTML = firstNumber / screen.innerHTML
+            break
+    }
+})

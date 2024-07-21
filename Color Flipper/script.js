41180c8a6f76376b29ad9fbcfa444c42b5679c0f
HWIBUH <evoriusvalens@gmail.com>
2024-07-21 21:14:04 +0700

    Color Flipper

---
+++ Color Flipper/script.js
@@ -0,0 +1,7 @@
+//let body_color = document.querySelector("body")
+//let red = Math.floor(Math.random()*255)+1
+let copy_button = document.querySelector(".copy")
+
+copy_button.addEventListener('click', () => {
+    console.log("ga")
+})
